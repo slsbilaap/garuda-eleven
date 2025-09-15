c474ba3d78d2852ef97518eddae644c1868f0081
slsbilaap <aulyasalsabilaputri1@gmail.com>
2025-09-16 01:04:48 +0700

    done?

--- garuda_eleven/settings.py
+++ garuda_eleven/settings.py
@@ -35,7 +35,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "msy-aulya-garudaeleven.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://msy-aulya-garudaeleven.pbp.cs.ui.ac.id"
+    "https://msy-aulya-garudaeleven.pbp.cs.ui.ac.id/"
 ]
 
 # Application definition
